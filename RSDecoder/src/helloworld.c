fbda8bd80d6804977692b5144dbd38e61a966562
Vincent Lobo <vincelobo4299@gmail.com>
2020-08-13 02:23:35 -0500

    Changed a lot
    Copied a lot from https://github.com/Xilinx/embeddedsw/blob/master/XilinxProcessorIPLib/drivers/axidma/examples/xaxidma_example_simple_poll.c. Will test it tomorrow

--- RSDecoder/src/helloworld.c
+++ RSDecoder/src/helloworld.c
@@ -1,155 +1,146 @@
-/******************************************************************************
-*
-* Copyright (C) 2009 - 2014 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
-*
-******************************************************************************/
-
-/*
- * helloworld.c: simple test application
- *
- * This application configures UART 16550 to baud rate 9600.
- * PS7 UART (Zynq) is not initialized by this application, since
- * bootrom/bsp configures it to baud rate 115200
- *
- * ------------------------------------------------
- * | UART TYPE   BAUD RATE                        |
- * ------------------------------------------------
- *   uartns550   9600
- *   uartlite    Configurable only in HW design
- *   ps7_uart    115200 (configured by bootrom/bsp)
- */
-
-#include <stdio.h>
-#include "platform.h"
-#include "xil_printf.h"
 #include "xaxidma.h"
-#include "xintc.h"
+#include "xparameters.h"
+#include "xdebug.h"
 
+#if defined(XPAR_UARTNS550_0_BASEADDR)
+#include "xuartns550_l.h"       /* to use uartns550 */
+#endif
 
-#define MEM_BASE_ADDR		0x80000000						//Can be found in design_1_wrapper.xsa base address for the MIG
-#define TX_BUFFER_BASE		(MEM_BASE_ADDR + 0x00000010)
+/******************** Constant Definitions **********************************/
+#define DMA_DEV_ID		XPAR_AXIDMA_0_DEVICE_ID
 
-static void runPacket(void);
+#ifdef XPAR_AXI_7SDDR_0_S_AXI_BASEADDR
+#define DDR_BASE_ADDR		XPAR_AXI_7SDDR_0_S_AXI_BASEADDR
+#elif defined (XPAR_MIG7SERIES_0_BASEADDR)
+#define DDR_BASE_ADDR	XPAR_MIG7SERIES_0_BASEADDR
+#elif defined (XPAR_MIG_0_BASEADDR)
+#define DDR_BASE_ADDR	XPAR_MIG_0_BASEADDR
+#elif defined (XPAR_PSU_DDR_0_S_AXI_BASEADDR)
+#define DDR_BASE_ADDR	XPAR_PSU_DDR_0_S_AXI_BASEADDR
+#endif
 
-unsigned char testPacket[21];								//Initialize with values
-unsigned char decodedPacket[21];							//Initialize with values
-u32 *Packet = (u32 *) TX_BUFFER_BASE;
-int Status;
-volatile static int InterruptProcessed = FALSE;
+#ifndef DDR_BASE_ADDR
+#warning CHECK FOR THE VALID DDR ADDRESS IN XPARAMETERS.H, \
+		 DEFAULT SET TO 0x80000000
+#define MEM_BASE_ADDR		0x80000000
+#else
+#define MEM_BASE_ADDR		(DDR_BASE_ADDR + 0x1000000)
+#endif
 
-static XAxiDma myDMA;
-static XIntc InterruptController;
+#define TX_BUFFER_BASE		(MEM_BASE_ADDR + 0x00100000)
+#define RX_BUFFER_BASE		(MEM_BASE_ADDR + 0x00300000)
+#define RX_BUFFER_HIGH		(MEM_BASE_ADDR + 0x004FFFFF)
+
+#define MAX_PKT_LEN		0x20
+
+#define TEST_START_VALUE	0xC
+
+#define NUMBER_OF_TRANSFERS	4
+
+/************************** Function Prototypes ******************************/
+extern void xil_printf(const char *format, ...);
+
+int XAxiDma_SimplePollExample(u16 DeviceId);
+
+/************************** Variable Definitions *****************************/
+XAxiDma AxiDma;
+unsigned char testPacket[32] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,1,2,3,4,5,0,0,1,0,0,0};
 
 int main()
 {
-    init_platform();
-    XInterruptHandler DeviceDriverHandler;
+	int Status;
 
-    Status = XIntc_Initialize(&InterruptController, XPAR_AXI_INTC_0_DEVICE_ID);
+	xil_printf("\r\n--- Entering main() --- \r\n");
+
+	/* Run the poll example for simple transfer */
+	Status = XAxiDma_SimplePollExample(DMA_DEV_ID);
+
 	if (Status != XST_SUCCESS) {
-		print("initialize failed");
-		return XST_FAILURE;
-	}
-	Status = XIntc_SelfTest(&InterruptController);
-	if (Status != XST_SUCCESS) {
-		print("self test failed");
-		return XST_FAILURE;
-	}
-	Status = XIntc_Connect(&InterruptController, 0x0, DeviceDriverHandler, (void *)0);
-	if (Status != XST_SUCCESS) {
-		print("connect failed");
-		return XST_FAILURE;
-	}
-	Status = XIntc_Start(&InterruptController, XIN_REAL_MODE);
-	if (Status != XST_SUCCESS) {
-		print("start failed");
+		xil_printf("XAxiDma_SimplePoll Example Failed\r\n");
 		return XST_FAILURE;
 	}
 
-	XIntc_Enable(&InterruptController, 0x0);
+	xil_printf("Successfully ran XAxiDma_SimplePoll Example\r\n");
 
+	xil_printf("--- Exiting main() --- \r\n");
 
-    XAxiDma_CfgInitialize(&myDMA,XAxiDma_LookupConfigBaseAddr(0x41e00000));
+	return XST_SUCCESS;
 
-    XAxiDma_Start(&myDMA);
-
-    print("Hello World\n\r");
-
-    runPacket();
-
-    cleanup_platform();
-    return 0;
 }
 
-void runPacket()
+#if defined(XPAR_UARTNS550_0_BASEADDR)
+static void Uart550_Setup(void)
 {
-	UINTPTR bufAdr = 0xff;
+	XUartNs550_SetBaud(XPAR_UARTNS550_0_BASEADDR,
+			XPAR_XUARTNS550_CLOCK_HZ, 9600);
 
-	microblaze_enable_interrupts();
-	XAxiDma_IntrEnable(&myDMA, XAXIDMA_IRQ_IOC_MASK, XAXIDMA_DMA_TO_DEVICE);
-	XAxiDma_IntrEnable(&myDMA, XAXIDMA_IRQ_IOC_MASK, XAXIDMA_DEVICE_TO_DMA);
+	XUartNs550_SetLineControlReg(XPAR_UARTNS550_0_BASEADDR,
+			XUN_LCR_8_DATA_BITS);
 
-
-
-	Status = XAxiDma_SimpleTransfer(&myDMA,bufAdr,0x15,XAXIDMA_DMA_TO_DEVICE);
-	if(Status == XST_SUCCESS)
-		print("success of submission");
-	else if(Status == XST_FAILURE)
-		print("submission failure, maybe caused by:*Another simple transfer is still going");
-	else if(Status == XST_INVALID_PARAM)
-		print("if:Length out of valid range [1:8M] *address not aligned when DRE is not built in");
-	while (1) {
-		if (InterruptProcessed)
-			break;
-	}
-
-	// Wait for completion
-//	while (XAxiDma_Busy(&myDMA, XAXIDMA_DMA_TO_DEVICE)) { /* Wait */ }
-//	while (XAxiDma_Busy(&myDMA, XAXIDMA_DEVICE_TO_DMA)) { /* Wait */ }
-
-
-	Status = XAxiDma_SimpleTransfer(&myDMA,bufAdr,0x15,XAXIDMA_DEVICE_TO_DMA);
-	if(Status == XST_SUCCESS)
-		print("success of submission");
-	else if(Status == XST_FAILURE)
-		print("submission failure, maybe caused by:*Another simple transfer is still going");
-	else if(Status == XST_INVALID_PARAM)
-		print("if:Length out of valid range [1:8M] *address not aligned when DRE is not built in");
 }
+#endif
 
-void DeviceDriverHandler(void *CallbackRef)
+// The example to do the simple transfer through polling. The constant
+// NUMBER_OF_TRANSFERS defines how many times a simple transfer is repeated.
+
+int XAxiDma_SimplePollExample(u16 DeviceId)
 {
-	/*
-	 * Indicate the interrupt has been processed using a shared variable.
+	XAxiDma_Config *CfgPtr;
+	int Status;
+	int Tries = NUMBER_OF_TRANSFERS;
+	int Index;
+	u8 *TxBufferPtr;
+	u8 *RxBufferPtr;
+
+	TxBufferPtr = (u8 *)TX_BUFFER_BASE ;
+	RxBufferPtr = (u8 *)RX_BUFFER_BASE;
+
+	/* Initialize the XAxiDma device.
 	 */
-	InterruptProcessed = TRUE;
+	CfgPtr = XAxiDma_LookupConfig(DeviceId);
+	if (!CfgPtr) {
+		xil_printf("No config found for %d\r\n", DeviceId);
+		return XST_FAILURE;
+	}
 
+	Status = XAxiDma_CfgInitialize(&AxiDma, CfgPtr);
+	if (Status != XST_SUCCESS) {
+		xil_printf("Initialization failed %d\r\n", Status);
+		return XST_FAILURE;
+	}
+
+	if(XAxiDma_HasSg(&AxiDma)){
+		xil_printf("Device configured as SG mode \r\n");
+		return XST_FAILURE;
+	}
+
+	/* Disable interrupts, we use polling mode
+	 */
+	XAxiDma_IntrDisable(&AxiDma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DEVICE_TO_DMA);
+	XAxiDma_IntrDisable(&AxiDma, XAXIDMA_IRQ_ALL_MASK, XAXIDMA_DMA_TO_DEVICE);
+
+	for(int i = 0; i < MAX_PKT_LEN; i++) {
+			TxBufferPtr[i] = testPacket[i];
+	}
+
+	for(Index = 0; Index < Tries; Index ++) {
+		Status = XAxiDma_SimpleTransfer(&AxiDma,(UINTPTR) RxBufferPtr,
+					MAX_PKT_LEN, XAXIDMA_DEVICE_TO_DMA);
+
+		if (Status != XST_SUCCESS) {
+			return XST_FAILURE;
+		}
+
+		Status = XAxiDma_SimpleTransfer(&AxiDma,(UINTPTR) TxBufferPtr, MAX_PKT_LEN, XAXIDMA_DMA_TO_DEVICE);
+
+		if (Status != XST_SUCCESS) {
+			return XST_FAILURE;
+		}
+
+		while ((XAxiDma_Busy(&AxiDma,XAXIDMA_DEVICE_TO_DMA)) ||
+			(XAxiDma_Busy(&AxiDma,XAXIDMA_DMA_TO_DEVICE))) {
+				/* Wait */
+		}
+	}
+	return XST_SUCCESS;
 }
-
-
