57484d01d6d82decdee68d7824f9e7e17aacbd3b
Vincent Lobo <vincelobo4299@gmail.com>
2020-08-12 23:26:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RSDecoderSW
+ Vince's Vitis workspace for RSDecoder. Platform, system, and application for the RSDecoder hardware
---
+++ RSDecoder/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters.h \
+ C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_cache.h \
+ C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/mb_interface.h \
+ C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_types.h \
+ C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_assert.h \
+ C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_exception.h \
+ C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters.h \
+ ../src/platform_config.h
+
+C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_cache.h:
+
+C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/mb_interface.h:
+
+C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_types.h:
+
+C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_assert.h:
+
+C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xil_exception.h:
+
+C:/Users/vince/VitisWorkspace/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_domain/bspinclude/include/xparameters.h:
+
+../src/platform_config.h:
